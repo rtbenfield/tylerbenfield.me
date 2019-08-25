9a1d1e4077e4881b3d9a60d4823ec4a9bf17a091
Tyler Benfield <rtbenfield@gmail.com>
2019-08-25 10:46:57 -0400

    Enable service worker (#2)
    * Enabled service worker in Gatsby
    
    * Updated dependencies

--- gatsby-config.js
+++ gatsby-config.js
@@ -34,8 +34,6 @@ module.exports = {
         trackingId: `UA-81673230-3`,
       },
     },
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
+    `gatsby-plugin-offline`,
   ],
 };
