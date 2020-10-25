8eaeb8c799db49295e0c10b85c0c4eb37a8ffd3f
Tyler Benfield <rtbenfield@gmail.com>
2020-10-25 12:04:07 -0400

    Adding Google Analytics tag

--- src/pages/_document.jsx
+++ src/pages/_document.jsx
@@ -7,6 +7,20 @@ export default class MyDocument extends Document {
         <Head>
           <link rel="manifest" href="/manifest.webmanifest" />
           <meta name="theme-color" content="#81a1c1" />
+          <script
+            async
+            src="https://www.googletagmanager.com/gtag/js?id=G-76GBQWVY52"
+          ></script>
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+
+gtag('config', 'G-76GBQWVY52');`,
+            }}
+          ></script>
         </Head>
         <body>
           <Main />
